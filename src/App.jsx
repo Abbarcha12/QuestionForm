30ed94ac4472ad0f0da4c6e64e7baaa7871acf9c
Barcha1 <abbarcha123456@gmail.com>
2024-04-09 22:41:44 +0500

    second update

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,20 @@ import ImageThree from "./assets/3.png";
 import ImageFour from "./assets/1.png";
 import ImageFive from "./assets/5.jpg";
 import ImageSix from "./assets/6.jpg";
+import pack1 from "./assets/pack1.png";
+import pack2 from "./assets/pack2.png";
+import pack3 from "./assets/pack3.png";
+import pack4 from "./assets/pack4.png";
+import pack5 from "./assets/pack5.png";
+import pack6 from "./assets/pack6.png";
+import pack7 from "./assets/pack7.png";
+import pack8 from "./assets/pack8.png";
+import pack9 from "./assets/pack9.png";
 
 const App = () => {
   const [answers, setAnswers] = useState(Array(8).fill(""));
   const [suggestedPackage, setSuggestedPackage] = useState("");
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(-1);
   const [showPackgae, setshowPackgae] = useState(true);
 
   const backgroundImages = [
@@ -47,6 +56,7 @@ const App = () => {
     {
       name: "Exam and Midterm Package",
       Link: "https://mydormstore.ca/products/exam-care-kit",
+      img: pack1,
       keywords: [
         "group study",
         "studying together",
@@ -63,6 +73,7 @@ const App = () => {
     },
     {
       name: "Thinking of You Care Package",
+      img: pack7,
       Link: "https://mydormstore.ca/products/thinking-of-you-care-package",
       keywords: [
         "empathy",
@@ -78,6 +89,7 @@ const App = () => {
     },
     {
       name: "Under the Weather Package",
+      img: pack8,
       Link: "https://mydormstore.ca/products/get-well-soon-care-kit",
       keywords: [
         "sickness",
@@ -94,6 +106,7 @@ const App = () => {
     },
     {
       name: "Build Your Own Care Package",
+      img: pack9,
       Link: "https://mydormstore.ca/products/build-your-own-care-package",
       keywords: [
         "hobbies",
@@ -109,6 +122,7 @@ const App = () => {
     {
       name: "Sleep Well Care Package",
       Link: "https://mydormstore.ca/products/sleep-well-care-package",
+      img: pack6,
       keywords: [
         "relaxation",
         "sleep",
@@ -123,6 +137,7 @@ const App = () => {
     {
       name: "Birthday Care Package",
       Link: "https://mydormstore.ca/products/birthday-care-package",
+      img: pack2,
       keywords: [
         "birthdays",
         "celebrations",
@@ -137,6 +152,7 @@ const App = () => {
     {
       name: "Valentine's Package",
       Link: "https://mydormstore.ca/products/valentines-day-care-package",
+      img: pack3,
       keywords: [
         "Valentine's Day",
         "friendship",
@@ -148,6 +164,7 @@ const App = () => {
     },
     {
       name: "Easter Package",
+      img: pack5,
       Link: "https://mydormstore.ca/products/easter-care-package",
       keywords: [
         "Easter",
@@ -160,6 +177,7 @@ const App = () => {
     },
     {
       name: "Super Bowl Care Package",
+      img: pack4,
       Link: "https://mydormstore.ca/products/super-bowl-care-package",
       keywords: [
         "Super Bowl",
@@ -214,16 +232,18 @@ const App = () => {
       setSuggestedPackage([
         packages[suggestedPackageIndex].name,
         packages[suggestedPackageIndex].Link,
+        packages[suggestedPackageIndex].img,
       ]);
       setshowPackgae(true);
     } else {
       setSuggestedPackage("No package suggested");
+      setshowPackgae(false);
     }
+    console.log(suggestedPackage);
   };
   useEffect(() => {
     setshowPackgae(false);
   }, []);
-  console.log(suggestedPackage);
   return (
     <motion.div
       initial={{ y: 10, opacity: 0 }}
@@ -246,15 +266,15 @@ const App = () => {
           transition={{ duration: 1 }}>
           <img src={Logo} className="Logo" alt="Logo" />
           <Container>
-            <Grid
-              container
-              spacing={2}
-              justifyContent={"center"}
-              alignItems={"center"}
-              mt={10}>
-              {showPackgae ? (
-                <Grid item xs={11} sm={8}>
-                  <Card sx={{ backgroundColor: "#ebf6fd" }}>
+            {index == -1 ? (
+              <Grid
+                container
+                spacing={2}
+                justifyContent={"center"}
+                alignItems={"center"}
+                mt={10}>
+                <Grid item xs={11} sm={5} md={4}>
+                  <Card sx={{ backgroundColor: "#ebf6fd", height: "300px" }}>
                     <CardContent>
                       <AnimatePresence>
                         <motion.div
@@ -266,27 +286,31 @@ const App = () => {
                             display="flex"
                             justifyContent={"space-between"}
                             justifyItems={"center"}
-                            mt={4}
-                            mb={4}>
+                            alignItems={"center"}
+                            textAlign={"center"}>
+                            <h1 className="title">Dorm Room</h1>
                             <Typography
+                              mb={4}
                               style={{
-                                fontWeight: "700",
-                                fontSize: "28px",
+                                fontSize: "18px",
                               }}
                               variant="h1">
-                              {suggestedPackage[0]}
+                              Find your personalized dorm room packages
                             </Typography>
                             <Button
                               href={suggestedPackage[1]}
                               style={{
                                 backgroundColor: "#0e1b4d",
                                 borderRadius: "40px",
-                                padding: ".5rem 2rem",
+                                padding: ".2rem 1rem",
+                                textAlign: "center",
+                                width: "50%",
                                 color: "#fff",
                                 textTransform: "capitalize",
-                                fontSize: "18px",
-                              }}>
-                              Add to cart
+                                fontSize: "16px",
+                              }}
+                              onClick={handleNext}>
+                              Starts
                             </Button>
                           </Box>
                         </motion.div>
@@ -294,100 +318,159 @@ const App = () => {
                     </CardContent>
                   </Card>
                 </Grid>
-              ) : (
-                <Grid item xs={12} sm={10} key={index}>
-                  <motion.div
-                    initial={{ y: 10, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    exit={{ y: -10, opacity: 0 }}
-                    transition={{ duration: 2 }}>
-                    <Card sx={{ backgroundColor: "#ebf6fd" }}>
+              </Grid>
+            ) : (
+              <Grid
+                container
+                spacing={2}
+                justifyContent={"center"}
+                alignItems={"center"}>
+                {showPackgae ? (
+                  <Grid item xs={11} sm={8} md={4}>
+                    <Card sx={{ backgroundColor: "#ffffff" }}>
                       <CardContent>
-                        <Box
-                          display={"flex"}
-                          justifyContent={"space-between"}
-                          alignItems={"center"}
-                          position={"relative"}
-                          mb={1}>
-                          <Typography
-                            style={{ color: "#0e1b4d", fontSize: "18px" }}>
-                            {questions[index]}
-                          </Typography>
-                        </Box>
                         <AnimatePresence>
                           <motion.div
                             initial={{ y: 10, opacity: 0 }}
                             animate={{ y: 0, opacity: 1 }}
                             exit={{ y: -10, opacity: 0 }}
                             transition={{ duration: 0.2 }}>
-                            <TextField
-                              placeholder="Your Answer"
-                              size="medium"
-                              fullWidth
-                              sx={{
-                                backgroundColor: "#ebf6fd",
-                                marginTop: "10px",
-                                "& input::placeholder": {
-                                  color: "#0e1b4d",
-                                },
-                              }}
-                              value={answers[index]}
-                              onChange={(e) =>
-                                handleAnswer(index, e.target.value)
-                              }
-                            />
+                            <Box
+                              display="flex"
+                              justifyContent={"space-between"}
+                              flexDirection={"column"}
+                              justifyItems={"center"}
+                              textAlign={"center"}
+                              mb={4}>
+                              <img
+                                src={suggestedPackage[2]}
+                                alt="product"
+                                className="productimage"
+                              />
+                              <Typography
+                                style={{
+                                  margin: "20px 0px",
+                                  fontWeight: "700",
+                                  fontSize: "24px",
+                                }}
+                                variant="h1">
+                                {suggestedPackage[0]}
+                              </Typography>
+                              <Button
+                                href={suggestedPackage[1]}
+                                style={{
+                                  backgroundColor: "#0e1b4d",
+                                  borderRadius: "40px",
+                                  padding: ".2rem 0rem",
+                                  color: "#fff",
+
+                                  textTransform: "capitalize",
+                                  fontSize: "18px",
+                                }}>
+                                Add to cart
+                              </Button>
+                            </Box>
                           </motion.div>
                         </AnimatePresence>
-                        <Box
-                          mt={6}
-                          justifyContent={"space-between"}
-                          display={"flex"}>
-                          <Button
-                            style={{
-                              backgroundColor:
-                                index === 0 ? "#878787dd" : "#0e1b4d",
-                              borderRadius: "40px",
-                              padding: ".5rem 2rem",
-                              color: "#fff",
-                              textTransform: "capitalize",
-                            }}
-                            onClick={handleBack}
-                            disabled={index === 0}>
-                            Back
-                          </Button>
-                          {index === questions.length - 1 ? (
-                            <Button
-                              style={{
-                                backgroundColor: "#0e1b4d",
-                                borderRadius: "40px",
-                                padding: ".5rem 2rem",
-                                color: "#fff",
-                                textTransform: "capitalize",
-                              }}
-                              onClick={suggestPackage}>
-                              Suggest Package
-                            </Button>
-                          ) : (
-                            <Button
-                              style={{
-                                backgroundColor: "#0e1b4d",
-                                borderRadius: "40px",
-                                padding: ".5rem 2rem",
-                                color: "#fff",
-                                textTransform: "capitalize",
-                              }}
-                              onClick={handleNext}
-                              disabled={index === questions.length - 1}>
-                              Next
-                            </Button>
-                          )}
-                        </Box>
                       </CardContent>
                     </Card>
-                  </motion.div>
-                </Grid>
-              )}
-            </Grid>
+                  </Grid>
+                ) : (
+                  <Grid item xs={12} sm={10} key={index}>
+                    <motion.div
+                      initial={{ y: 10, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      exit={{ y: -10, opacity: 0 }}
+                      transition={{ duration: 2 }}>
+                      <Card sx={{ backgroundColor: "#ebf6fd" }}>
+                        <CardContent>
+                          <Box
+                            display={"flex"}
+                            flexDirection={"column"}
+                            justifyContent={"space-between"}
+                            alignItems={"center"}
+                            position={"relative"}
+                            mb={1}>
+                            <Typography
+                              style={{ color: "#0e1b4d", fontSize: "18px" }}>
+                              {questions[index]}
+                            </Typography>
+                          </Box>
+                          <AnimatePresence>
+                            <motion.div
+                              initial={{ y: 10, opacity: 0 }}
+                              animate={{ y: 0, opacity: 1 }}
+                              exit={{ y: -10, opacity: 0 }}
+                              transition={{ duration: 0.2 }}>
+                              <TextField
+                                placeholder="Your Answer"
+                                size="medium"
+                                fullWidth
+                                sx={{
+                                  backgroundColor: "#ebf6fd",
+                                  marginTop: "10px",
+                                  "& input::placeholder": {
+                                    color: "#0e1b4d",
+                                  },
+                                }}
+                                value={answers[index]}
+                                onChange={(e) =>
+                                  handleAnswer(index, e.target.value)
+                                }
+                              />
+                            </motion.div>
+                          </AnimatePresence>
+                          <Box
+                            mt={6}
+                            justifyContent={"space-between"}
+                            display={"flex"}>
+                            <Button
+                              style={{
+                                backgroundColor:
+                                  index === 0 ? "#878787dd" : "#0e1b4d",
+                                borderRadius: "40px",
+                                padding: ".5rem 2rem",
+                                color: "#fff",
+                                textTransform: "capitalize",
+                              }}
+                              onClick={handleBack}
+                              disabled={index === 0}>
+                              Back
+                            </Button>
+                            {index === questions.length - 1 ? (
+                              <Button
+                                style={{
+                                  backgroundColor: "#0e1b4d",
+                                  borderRadius: "40px",
+                                  padding: ".5rem 2rem",
+                                  color: "#fff",
+                                  textTransform: "capitalize",
+                                }}
+                                onClick={suggestPackage}>
+                                Suggest Package
+                              </Button>
+                            ) : (
+                              <Button
+                                style={{
+                                  backgroundColor: "#0e1b4d",
+                                  borderRadius: "40px",
+                                  padding: ".5rem 2rem",
+                                  color: "#fff",
+                                  textTransform: "capitalize",
+                                }}
+                                onClick={handleNext}
+                                disabled={index === questions.length - 1}>
+                                Next
+                              </Button>
+                            )}
+                          </Box>
+                        </CardContent>
+                      </Card>
+                    </motion.div>
+                  </Grid>
+                )}
+              </Grid>
+            )}
           </Container>
         </motion.div>
       </Box>
